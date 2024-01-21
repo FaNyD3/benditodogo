a38654a67be34c7a73a0ce654e319c846b6149ca
dop <fany.fop@gmail.com>
2024-01-20 19:44:14 -0600

    add redirection to social media in footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import instagramLogo from './assets/icons/instagram.svg';
 import whatsappLogo from './assets/icons/whatsapp.svg';
 import mapPin from './assets/icons/map-pin.svg';
 
-
 function App() {
   const classes = useStyles({});
   return (
@@ -25,13 +24,19 @@ function App() {
               <img className={classes.footerLogo} src={logo}></img>
             </div>
             <div className={classes.Face}>
+            <a className={classes.text} href="https://www.facebook.com/profile.php?id=100063644673026&mibextid=LQQJ4d">
               <img src={facebookLogo} alt="facebook" /> bendito_dogo
+            </a>
             </div>
             <div className={classes.Insta}>
+            <a className={classes.text} href="https://www.instagram.com/bendito_dogo?igsh=MWJlemUzZDloZGN2cA==">
               <img src={instagramLogo} alt="instagram" /> bendito_dogo
+            </a>
             </div>
             <div className={classes.Whatsapp}>
-              <img src={whatsappLogo} alt="whatsapp" /> (33)14479481
+              <a className={classes.text} href="https://wa.me/message/ROHILSIU5KKIN1">
+                <img src={whatsappLogo} alt="whatsapp" /> Ordena aquí
+              </a>
             </div>
             <div className={classes.Domicilio}>
             <img src={mapPin} alt="mapPin" /> Avenida Patria 3586 Fracc. Revolucion Tlaquepaque, Jalisco
